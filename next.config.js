8611674a01760ef0359d5173122c422e662722bd
juliafealves <julia.alves@cogna.com.br>
2023-06-08 12:19:51 -0300

    Added support to PWA in the project.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const isProduction = process.env.NODE_ENV === 'production'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProduction
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
